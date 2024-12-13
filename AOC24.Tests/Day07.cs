a819e95609600c4f81d34cb97c670f1bf6a30871
Tim <timiscoding@gmail.com>
2024-12-13 16:28:01 +1100

    Add iterative solution and minor refactor for Day 7

--- AOC24.Tests/Day07.cs
+++ AOC24.Tests/Day07.cs
@@ -7,31 +7,45 @@ public class Day07Tests
 {
     private Day07.Operation[] _ops = [Day07.Add, Day07.Mul, Day07.Concat];
     
-    [TestCase(new ulong[] { 2, 3 }, 100U, false)]
-    [TestCase(new ulong[] { 1, 2 }, 3U, true)]      // add
-    [TestCase(new ulong[] { 2, 3 }, 6U, true)]      // mul
-    [TestCase(new ulong[] { 2, 3 }, 23U, true)]     // concat
-    public void CanSolve_TwoNums_ReturnsCorrectResult(ulong[] nums, ulong target, bool expected)
+    [TestCase(new long[] { 2, 3 }, 100, false)]
+    [TestCase(new long[] { 1, 2 }, 3, true)]      // add
+    [TestCase(new long[] { 2, 3 }, 6, true)]      // mul
+    [TestCase(new long[] { 2, 3 }, 23, true)]     // concat
+    public void CanSolve_TwoNums_ReturnsCorrectResult(long[] nums, long target, bool expected)
     {
         var actual = Day07.CanSolve(_ops, nums, target);
         Assert.That(actual, Is.EqualTo(expected));
     }
     
-    [TestCase(new ulong[] { 2, 3, 4 }, 100U, false)]
-    [TestCase(new ulong[] { 2, 3, 4 }, 9U, true)]      // 2+3+4
-    [TestCase(new ulong[] { 2, 3, 4 }, 20U, true)]     // 2+3*4
-    [TestCase(new ulong[] { 2, 3, 4 }, 54U, true)]     // 2+3||4
-    [TestCase(new ulong[] { 2, 3, 4 }, 24U, true)]     // 2*3*4
-    [TestCase(new ulong[] { 2, 3, 4 }, 10U, true)]     // 2*3+4
-    [TestCase(new ulong[] { 2, 3, 4 }, 64U, true)]     // 2*3||4
-    [TestCase(new ulong[] { 2, 3, 4 }, 234U, true)]    // 2||3||4
-    [TestCase(new ulong[] { 2, 3, 4 }, 27U, true)]      // 2||3+4
-    [TestCase(new ulong[] { 2, 3, 4 }, 92U, true)]      // 2||3*4
-    public void CanSolve_ThreeNums_ReturnsCorrectResult(ulong[] nums, ulong target, bool expected)
+    [TestCase(new long[] { 2, 3, 4 }, 100, false)]
+    [TestCase(new long[] { 2, 3, 4 }, 9, true)]      // 2+3+4
+    [TestCase(new long[] { 2, 3, 4 }, 20, true)]     // 2+3*4
+    [TestCase(new long[] { 2, 3, 4 }, 54, true)]     // 2+3||4
+    [TestCase(new long[] { 2, 3, 4 }, 24, true)]     // 2*3*4
+    [TestCase(new long[] { 2, 3, 4 }, 10, true)]     // 2*3+4
+    [TestCase(new long[] { 2, 3, 4 }, 64, true)]     // 2*3||4
+    [TestCase(new long[] { 2, 3, 4 }, 234, true)]    // 2||3||4
+    [TestCase(new long[] { 2, 3, 4 }, 27, true)]      // 2||3+4
+    [TestCase(new long[] { 2, 3, 4 }, 92, true)]      // 2||3*4
+    public void CanSolve_ThreeNums_ReturnsCorrectResult(long[] nums, long target, bool expected)
     {
         var actual = Day07.CanSolve(_ops, nums, target);
         Assert.That(actual, Is.EqualTo(expected));
     }
-    
-    
+
+    [TestCase(new long[] { 2, 3, 4 }, 100, false)]
+    [TestCase(new long[] { 2, 3, 4 }, 9, true)]      // 2+3+4
+    [TestCase(new long[] { 2, 3, 4 }, 20, true)]     // 2+3*4
+    [TestCase(new long[] { 2, 3, 4 }, 54, true)]     // 2+3||4
+    [TestCase(new long[] { 2, 3, 4 }, 24, true)]     // 2*3*4
+    [TestCase(new long[] { 2, 3, 4 }, 10, true)]     // 2*3+4
+    [TestCase(new long[] { 2, 3, 4 }, 64, true)]     // 2*3||4
+    [TestCase(new long[] { 2, 3, 4 }, 234, true)]    // 2||3||4
+    [TestCase(new long[] { 2, 3, 4 }, 27, true)]      // 2||3+4
+    [TestCase(new long[] { 2, 3, 4 }, 92, true)]      // 2||3*4
+    public void CanSolveIterative_ReturnsCorrectResult(long[] nums, long target, bool expected)
+    {
+        var actual = Day07.CanSolveIterative(_ops, nums, target);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
 }
--- AOC24/Solutions/Day07.cs
+++ AOC24/Solutions/Day07.cs
@@ -5,45 +5,64 @@ namespace AOC24.Solutions;
 
 public static class Day07
 {
-    public delegate ulong Operation(ulong a, ulong b);
+    public delegate long Operation(long a, long b);
     
     public static void Solve()
     {
         var lines = InputReader.GetLines("Day07.txt");
         var equations = lines.Select(line =>
         {
-            var nums = line.Split([':', ' '], StringSplitOptions.RemoveEmptyEntries).Select(ulong.Parse).ToArray();
+            var nums = line.Split([':', ' '], StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
             return new { Target = nums[0], Nums = nums[1..] };
         }).ToArray();
 
         List<Operation> ops = [Add, Mul];
-        var sum = equations.Where(e => CanSolve(ops, e.Nums, e.Target)).Aggregate(0UL, (sum, v) => sum + v.Target);
+        var sum = equations.Where(e => CanSolve(ops, e.Nums, e.Target)).Sum(e => e.Target);
         Console.WriteLine($"Day 7 Part 1: {sum} {sum:N0}");
         
-        sum = equations.Where(e => CanSolve(ops.Concat([ Concat ]), e.Nums, e.Target)).Aggregate(0UL, (sum, v) => sum + v.Target);
+        sum = equations.Where(e => CanSolve(ops.Concat([ Concat ]), e.Nums, e.Target)).Sum(e => e.Target);
         Console.WriteLine($"Day 7 Part 2: {sum} {sum:N0}");
     }
     
-        public static bool CanSolve(IEnumerable<Operation> ops, ulong[] nums, ulong target, ulong total = 0, int index = 1)
+    public static bool CanSolve(IEnumerable<Operation> ops, long[] nums, long target, long total = 0, int index = 1)
+    {
+        if (nums.Length < 2) throw new ArgumentException("nums must have at least 2 elements");
+        if (index == 1) total = nums[0];
+        if (total > target) return false;
+        if (index >= nums.Length)
         {
-            if (nums.Length < 2) throw new ArgumentException("nums must have at least 2 elements");
-            if (index == 1) total = nums[0];
-            if (total > target) return false;
-            if (index >= nums.Length)
-            {
-                return total == target;
-            }
-
-            foreach (var op in ops)
-            {
-                if (CanSolve(ops, nums, target, op(total, nums[index]), index + 1))
-                    return true;
-            }
-            return false;
+            return total == target;
         }
+
+        foreach (var op in ops)
+        {
+            if (CanSolve(ops, nums, target, op(total, nums[index]), index + 1))
+                return true;
+        }
+        return false;
+    }
+
+    public static bool CanSolveIterative(IEnumerable<Operation> ops, long[] nums, long target)
+    {
+        Queue<long> q = new();
+        q.Enqueue(nums[0]);
+        foreach (var n in nums.Skip(1))
+        {
+            var curQueueLen = q.Count;
+            for (var i = 0; i < curQueueLen; i++)
+            {
+                var subTotal = q.Dequeue();
+                foreach (var op in ops)
+                {
+                    q.Enqueue(op(subTotal, n));
+                }
+            }
+        }
+        return q.Any(total => total == target);
+    }
         
-    public static ulong Add(ulong num1, ulong num2) => num1 + num2;
-    public static ulong Mul(ulong num1, ulong num2) => num1 * num2;
-    public static ulong Concat(ulong num1, ulong num2) => ulong.Parse($"{num1}{num2}");
+    public static long Add(long num1, long num2) => num1 + num2;
+    public static long Mul(long num1, long num2) => num1 * num2;
+    public static long Concat(long num1, long num2) => long.Parse($"{num1}{num2}");
 
 }
